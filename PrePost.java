0731d965125bae423d0a4efd2738e72edfc10edb
Ohm0025 <toyohm0025@gmail.com>
2022-11-06 21:29:05 +0700

    new_java

---
+++ PrePost.java
@@ -0,0 +1,8 @@
+public class PrePost {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println("ค่า a เริ่มต้น "+a);
+        System.out.println("-postfix : "+(a--));
+        System.out.println("ค่า a ตอนนี้ "+a);
+    }
+}
